742ec77e5f1c577ae697cd54345f1311243f46e2
Shlomi <mail_s@walla.com>
2015-12-18 22:34:22 +0200

    exit

--- nextPage.js
+++ nextPage.js
@@ -1,8 +1,15 @@
 // JavaScript source code
 window.onload = function () {
     document.getElementById('prevPage').addEventListener("click", prevPage, false)
+    document.getElementById('exit').addEventListener("click", exit, false)
 }
 
 function prevPage() {
     location.assign("index.html");
+}
+
+function exit() {
+    //navigator.app.exitApp();
+    //window.exit(); // doesn't work in desktop chrome
+    window.close();
 }
