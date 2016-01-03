eb1c51666d6b1d9b3e0bb1eec320fe60ce9a6336
Shlomi <mail_s@walla.com>
2016-01-03 21:47:21 +0200

    screen size

--- nextPage.js
+++ nextPage.js
@@ -2,6 +2,9 @@
 window.onload = function () {
     document.getElementById('prevPage').addEventListener("click", prevPage, false)
     document.getElementById('exit').addEventListener("click", exit, false)
+
+    document.getElementById('width').innerText += window.innerWidth
+    document.getElementById('height').innerText += window.innerHeight
 }
 
 function prevPage() {
@@ -9,7 +12,9 @@ function prevPage() {
 }
 
 function exit() {
-    //navigator.app.exitApp();
+    navigator.app.exitApp(); // may need <script type="text/javascript" charset="utf-8" src="cordova-1.5.0.js"></script>
+    //app.exitApp();
+    //return exec(null, null, APP_PLUGIN_NAME, "exitApp", []);
     //window.exit(); // doesn't work in desktop chrome
-    window.close();
+    // window.close(); // works in desktop chrome but not android
 }
